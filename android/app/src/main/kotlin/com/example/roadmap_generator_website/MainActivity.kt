a85f2f25ab020743fe5bcb4c509fdef277fc0144
Grzegorz Niespodziany <grzegorz.niespodzianek@gmail.com>
2024-06-22 12:09:55 +0200

    init rest

---
+++ android/app/src/main/kotlin/com/example/roadmap_generator_website/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.roadmap_generator_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ui';
+
+
+   const mainActionColor = Color(0xFFFAF33E);
+   const monoButtonTextColor = Color(0xFF000000);
+   const buttonFillColor = Color(0xFFD9D9D9);
+   const pageBackgroundColor = Color(0xFF464655);
+   const secondaryColor = Color(0xFF3EFA95);
+   const titleTextColor = Color(0xFFFFFFFF);
